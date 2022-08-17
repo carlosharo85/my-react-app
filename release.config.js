cce4b57f10da546410905662a555b0c222e9198f
Carlos Haro <carlosharo8530@gmail.com>
2022-08-17 11:51:03 -0500

    docs(readme.md): badge
    Badge and status

--- release.config.js
+++ release.config.js
@@ -4,7 +4,11 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/npm",
-        "@semantic-release/github",
+        [
+            "@semantic-release/github",
+            {
+                assets: "dist/*.tgz",
+            },
+        ],
     ],
 };
